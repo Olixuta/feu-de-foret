3f5b054f6c61b42d5f00ae200d4606e4ae0546f7
Olixuta <dorianpadovan@gmail.com>
2025-07-13 11:18:51 +0200

    Add files via upload

---
+++ foretV02.py
@@ -0,0 +1,190 @@
+from random import choice,randint
+
+class Meteo:
+    def __init__(self) -> None:
+        self.orage=False
+        self.pluie=False
+        self.secheresse=False
+
+    def change_meteo(self,saisons):
+        if self.secheresse and saisons.nom=="été":
+            return None
+        else:
+            self.secheresse=False
+        proba=randint(1,100)
+        if saisons.nom=='hiver' or saisons.nom=='printemps':
+            if proba<17:
+                self.pluie=True
+            else:
+                self.pluie=False
+        elif saisons.nom=='automne':
+            if proba<=50:
+                self.pluie=True
+            else:
+                self.pluie=False
+        else:
+            if proba<=5:
+                self.orage=True
+                self.pluie=True
+            else:
+                self.orage,self.pluie=False,False
+            if proba>60:
+                self.secheresse=True
+    
+            
+class Vent:
+    def __init__(self,orientation,vitesse) -> None:
+        self.orientation=orientation
+        self.vitesse=vitesse
+    def changer_orientation(self):
+        orientations=["N","S","E","O","NE","NO","SO","SE"]
+        self.orientation=choice(orientations)
+    def changer_vitesse(self):
+        self.vitesse=randint(0,3)
+    def changer_vent(self,saisons):
+        self.changer_vitesse()
+        if saisons.nom == "hiver":
+            self.orientation="N"
+        else:
+            self.changer_orientation()
+
+
+
+dvent1={
+    'NO':[(-1,-1)],
+    'N':[(-1,0)],
+    'NE':[(-1,1)],
+    'O':[(0,-1)],
+    'E':[(0,1)],
+    'SO':[(1,-1)],
+    'S':[(1,0)],
+    'SE':[(1,1)]   
+}
+
+dvent2={
+    'NO':[(-1,-1),(-2,-2),(-1,-2),(-2,-1)],
+    'N':[(-1,0),(-2,0),(-2,-1),(-2,1)],
+    'NE':[(-1,1),(-2,1),(-2,2),(-1,2)],
+    'O':[(0,-1),(0,-2),(-1,-2),(1,-2)],
+    'E':[(0,1),(0,2),(-1,2),(1,2)],
+    'SO':[(1,-1),(2,-2),(1,-2),(2,-1)],
+    'S':[(1,0),(2,0),(2,-1),(2,1)],
+    'SE':[(1,1),(2,1),(2,2),(1,2)]   
+}
+
+dvent3={
+    'NO':[(-1,-1),(-2,-2),(-1,-2),(-2,-1),(-3,-2),(-2,-3)],
+    'N':[(-1,0),(-2,0),(-2,-1),(-2,1),(-3,0),(-3,-1),(-3,1)],
+    'NE':[(-1,1),(-2,1),(-2,2),(-1,2),(-3,2),(2,-3)],
+    'O':[(0,-1),(0,-2),(-1,-2),(1,-2),(0,-3),(-1,-3),(1,-3)],
+    'E':[(0,1),(0,2),(-1,2),(1,2),(0,3),(-1,3),(1,3)],
+    'SO':[(1,-1),(2,-2),(1,-2),(2,-1),(3,-2),(2,-3)],
+    'S':[(1,0),(2,0),(2,-1),(2,1),(3,0),(3,-1),(3,1)],
+    'SE':[(1,1),(2,1),(2,2),(1,2),(3,2),(2,3)]   
+}
+liste_vent=[dvent1,dvent2,dvent3]
+
+class Arbre:
+    def __init__(self, age ):
+        assert type(age)==int and age>=0 
+        self.age=age 
+        self.feu=False# feu
+        self.duree_flamme=0
+        self.etat="enfant" # peut prendre en valeur "enfant","adulte", "mort", "terre"
+        self.age_sup={"enfant":(5,"adulte"),"adulte":(300,"mort"),"mort":(350,"terre")}
+        
+    def prend_feu(self):
+        self.feu=True
+    
+    def eteind_feu(self):
+        self.feu=False
+        self.etat="mort"
+        self.duree_flamme=0
+
+    def propagation_depart_feu(self,meteo,cord_arbre,taille,grille):
+        if meteo.secheresse and self.etat=="mort": 
+            if randint(1,1000)<=25:
+                self.prend_feu()
+                return True
+        if self.feu:
+            if randint(1,3)==1:
+                l=self.positionsArbreVoisins(taille,grille,cord_arbre[0],cord_arbre[1])
+                if l!=[]:
+                    return choice(l)
+        return None # le feu ne se propage pas
+
+    def ajout_age(self):
+        self.age=self.age+1
+        if self.age>self.age_sup[self.etat][0]:
+            self.etat=self.age_sup[self.etat][1]
+            if self.etat=="terre":
+                return True
+        if self.feu==True:
+            self.duree_flamme+=1
+            if self.duree_flamme>3:
+                self.etat="terre"
+                return True
+        return False
+        
+        
+
+    def cord_valable(self,cord,grille,vent,taille):
+
+        L=[]
+        if vent.vitesse==0:
+            return [] #il n'aura pas d'enfant car la graine sera sur la même case que l'arbre
+        else:
+            for tuple in liste_vent[vent.vitesse-1][vent.orientation]:
+                if (taille>(cord[0]+tuple[0])>=0 and taille>(cord[1]+tuple[1])>=0) and grille[cord[0]+tuple[0]][cord[1]+tuple[1]]==None:
+                    #print(1)
+                    L.append(tuple)
+        return L
+    
+    def enfant(self,vent,grille,cord_arbre,taille):
+        """plante une graine
+        in:
+        vent(tuple): 1ere valeur str représentant le sens du vent et 2e valeur un entier entre 0 et 3 représentnt la puissance du vent
+        grille(liste de liste): représentant la forêt 
+        cord_arbre(tuple): contenant l'indice i et j de l'arbre dans la grille 
+        out:
+        None si la graine arrive en dehors de la forêt ou sur un arbre
+        sinon renvoie les coordonnées de l'enfant de l'arbre 
+        """
+        assert vent.vitesse>=0 and vent.vitesse<=3
+        if self.etat=="adulte":
+
+            l=self.cord_valable(cord_arbre,grille,vent,taille)
+            if l!=[]:
+
+                cord=choice(l)
+                if grille[cord[0]][cord[1]]==None:
+
+                    return (cord_arbre[0]+cord[0],cord_arbre[1]+cord[1])
+        return None
+    
+    def positionsArbreVoisins(self,taille,grille,i,j):
+        l=[]
+        pos=[
+            (i-1,j-1),(i-1,j),(i-1,j+1),
+            (i,j-1),           (i,j+1),
+            (i+1,j-1),(i+1,j),(i+1,j+1)
+        ]
+        for tuple in pos:
+            if taille>tuple[0]>=0 and taille>tuple[1]>=0 and grille[tuple[0]][tuple[1]].__class__==Arbre:
+                l.append(tuple)
+        return l
+                    
+
+
+
+
+
+
+
+
+
+        
+
+        
+        
+        
---
+++ simulationV03.py
@@ -0,0 +1,233 @@
+import pygame
+from pygame.locals import*
+from math import *
+from foretV01 import Arbre,Vent,Meteo
+from random import *
+
+pygame.init()
+
+tour_de_vie_monde=0
+
+vent = Vent("E",0)
+meteo=Meteo()
+
+
+# saison : les arbre ne peuvent faire des enfant que le printemp:
+class Saison:
+    def __init__(self,nom,couleur) -> None:
+        self.nom=nom
+        self.couleur=couleur
+        self.suivante=None
+
+liste_saison=['hiver','printemps','été','automne']
+liste_saison_couleur=["#d0e501","#d0e501","#d0e501","#d0e501",
+                      "#d0e501","#d0e501","#d0e501","#d0e501",
+                      "#d0e501","#d0e501","#d0e501","#d0e501"
+                      ,"#d0e501","#d0e501","#d0e501","#d0e501"]
+saisons=Saison('hiver',"#ffffff")
+saison1=saisons
+for i in range(len(liste_saison_couleur)):
+    saisons.suivante=Saison(liste_saison[i//4],liste_saison_couleur[i])
+    saisons=saisons.suivante
+saisons.suivante=saison1.suivante
+
+
+
+font = pygame.font.SysFont(None, 24)
+
+screen_width = 500
+screen= pygame.display.set_mode((screen_width+400,screen_width))
+pygame.display.set_caption('simulation forêt')
+
+dico_mode = {"arbre":"feu","feu":"pause","pause":"arbre"}
+mode = "arbre"
+vitesse=100
+nb_case=200
+taille_case=screen_width/nb_case
+
+# matrice du monde :
+
+monde=[[None for i in range(nb_case)]for j in range(nb_case)]
+
+# affichage
+
+def actualiser_premiere_fois():
+    pygame.draw.rect(screen,pygame.Color(saisons.couleur),(0,0,screen_width,screen_width))
+    pygame.draw.rect(screen,pygame.Color("white"),(screen_width,0,400,screen_width))
+    for i in range(1,nb_case):
+        pygame.draw.aaline(screen,"black",(i*taille_case,0),(i*taille_case,screen_width))
+        pygame.draw.aaline(screen,"black",(0,i*taille_case),(screen_width,i*taille_case))
+
+
+actualiser_premiere_fois()
+pygame.display.update()
+
+def afficher_arbre(coord,etat):
+    vrais_coord=[(coord[0]*taille_case,coord[1]*taille_case)]
+    vrais_coord.append((vrais_coord[0][0],vrais_coord[0][1]+taille_case))
+    vrais_coord.append((vrais_coord[0][0]+taille_case,vrais_coord[0][1]+taille_case))
+    vrais_coord[0]=(vrais_coord[0][0]+taille_case//2,vrais_coord[0][1])
+    if etat=="mort":
+        pygame.draw.polygon(screen,pygame.Color("#431c02"),vrais_coord)
+    elif etat=='feu':
+        pygame.draw.polygon(screen,pygame.Color("#fd0000"),vrais_coord)
+    else:
+        pygame.draw.polygon(screen,pygame.Color("#00bb25"),vrais_coord)
+
+def afficher_graine(coord):
+    vrais_coord=(coord[0]*taille_case+taille_case//2,coord[1]*taille_case+taille_case//2)
+    pygame.draw.circle(screen,pygame.Color("#e95c00"),vrais_coord,taille_case//4)
+
+# actualisation de l'affichage
+
+def actualiser():
+    actualiser_premiere_fois()
+    for i in range(nb_case):
+        for j in range(nb_case):
+            if monde[i][j].__class__==Arbre:
+                if monde[i][j].feu==False:
+                    if monde[i][j].etat=="adulte":
+                        afficher_arbre((i,j),"vivant")
+                    elif monde[i][j].etat=="mort":
+                        afficher_arbre((i,j),"mort")
+                    elif monde[i][j].etat=="terre":
+                        monde[i][j]=None   
+                    else:
+                        afficher_graine((i,j))
+                else:
+                    afficher_arbre((i,j),"feu")
+
+    # donné affiché
+    text = font.render('tour : '+str(tour_de_vie_monde),True,"Black")
+    screen.blit(text,(screen_width,0))
+    text = font.render('saison : '+saisons.nom,True,"Black")
+    screen.blit(text,(screen_width,20))
+    text = font.render("vent : orientation : "+ vent.orientation +" vitesse : "+str(vent.vitesse),True,"Black")
+    screen.blit(text,(screen_width,40))
+    text = font.render("vitesse simulation : "+ str(vitesse),True,"Black")
+    screen.blit(text,(screen_width,60))
+    text = font.render("mode : "+ mode,True,"Black")
+    screen.blit(text,(screen_width,80))
+    pygame.display.update()
+
+
+
+# boucle de la simulation
+temp_clique=pygame.time.get_ticks()
+temp_tour=pygame.time.get_ticks()
+start=True
+while start:
+    for event in pygame.event.get():
+            # quitter la simulation
+            if event.type == pygame.QUIT:
+                start=False
+
+            
+            if event.type== pygame.KEYDOWN:
+                # augmenté la vitesse ou baisser la vitesse de la simulation
+                if event.key == K_q:
+                    vitesse=vitesse*2
+                    actualiser()
+                if event.key == K_d:
+                    if vitesse>1:
+                        vitesse=vitesse//2
+                        actualiser()
+                # changer de mode :
+                if event.key == K_s:
+                    mode=dico_mode[mode]
+                    actualiser()
+                if event.key == K_SPACE:
+                    if mode != "pause":
+                        mode="pause"
+                    else:
+                        mode="arbre"
+                    actualiser()
+
+    # mettre un arbre
+    if pygame.mouse.get_pressed(num_buttons=3)[0] and pygame.time.get_ticks()-temp_clique>100:
+        temp_clique=pygame.time.get_ticks()
+        pos=pygame.mouse.get_pos()
+        coord=(round(pos[0]//taille_case),round(pos[1]//taille_case))
+        if -1<coord[0]<nb_case and -1<coord[1]<nb_case:
+            monde[coord[0]][coord[1]]=Arbre(0)
+        actualiser()
+
+    # mettre le feu à un arbre
+    if pygame.mouse.get_pressed(num_buttons=3)[2] and pygame.time.get_ticks()-temp_clique>100:
+        temp_clique=pygame.time.get_ticks()
+        pos=pygame.mouse.get_pos()
+        coord=(round(pos[0]//taille_case),round(pos[1]//taille_case))
+        if -1<coord[0]<nb_case and -1<coord[1]<nb_case:
+            if monde[coord[0]][coord[1]].__class__==Arbre:
+                if monde[coord[0]][coord[1]].feu==False:
+                    monde[coord[0]][coord[1]].prend_feu()
+        print(coord)
+        actualiser()
+
+
+    # mode simulation de croissance de forêt
+    if mode == "arbre":
+        if pygame.time.get_ticks()-temp_tour>vitesse:
+            # changement des variable
+            temp_tour=pygame.time.get_ticks()
+            tour_de_vie_monde+=1
+
+            # change de saison
+            saisons=saisons.suivante
+            # change vent
+            vent.changer_vent(saisons)
+            # change méteo
+            meteo.change_meteo(saisons)
+
+            for i in range(nb_case):
+                for j in range(nb_case):
+                    if monde[i][j].__class__ == Arbre:
+                        
+                        # feu
+                        coord_feu=monde[i][j].propagation_depart_feu(meteo,(i,j),nb_case,monde)
+                        if coord_feu:
+                            mode="feu"
+
+                        # fait des enfants
+                        if saisons.nom == "printemps" and monde[i][j].etat=="adulte":
+                            if randint(0,3)==0:
+                                coord_graine=monde[i][j].enfant(vent,monde,(i,j),nb_case)
+                                if coord_graine != None:
+                                    monde[coord_graine[0]][coord_graine[1]]=Arbre(0)
+                                    
+                        # l'arbre grandi
+                        if monde[i][j].ajout_age():
+                            monde[i][j]=None
+            # change l'affichage
+            actualiser()
+
+        # mode simulation d'incendie
+    elif mode == "feu":
+        if pygame.time.get_ticks()-temp_tour>vitesse:
+            temp_tour=pygame.time.get_ticks()
+            # change vent
+            vent.changer_vent(saisons)
+            # compteur des arbre en feu
+            compteur=0
+            for i in range(nb_case):
+                for j in range(nb_case):
+                    if monde[i][j].__class__ == Arbre:
+                            
+                        # feu se propage ou apparait
+                        coord_feu=monde[i][j].propagation_depart_feu(meteo,(i,j),nb_case,monde)
+                        if coord_feu!=None and coord_feu!=True:
+                            monde[coord_feu[0]][coord_feu[1]].prend_feu()
+
+                        if monde[i][j].feu:
+                            compteur+=1
+                            
+
+                        # le feu brule l'arbre
+                        if monde[i][j].tour_feu():
+                            monde[i][j]=None
+            if compteur==0:
+                mode = "arbre"
+
+            # change l'affichage
+            actualiser()
+
